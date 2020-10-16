52332082601e4a215fba2684ace281fae84b4d2e
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-10-16 18:35:49 +0500

    add(agora): init func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AgoraRTC from 'agora-rtc-sdk'
 
-function App() {
+// rtc object
+var rtc = {
+  client: null,
+  joined: false,
+  published: false,
+  localStream: null,
+  remoteStreams: [],
+  params: {},
+}
+
+// Options for joining a channel
+var option = {
+  appID: '818ec1aee6604710974eb1c9a639a76b',
+  channel: 'test-channel',
+  uid: null,
+  token: '006818ec1aee6604710974eb1c9a639a76bIACzVBbIZPz3CUPWIIK+SQisroEIkSq5zXYae1HUh8vxm2LMzZAAAAAAEABEZ8flL9CKXwEAAQAv0Ipf',
+}
+
+function App () {
+
+  useEffect(() => {
+    // Create a client
+    rtc.client = AgoraRTC.createClient({ mode: 'rtc', codec: 'h264' })
+
+    // Initialize the client
+    rtc.client.init(option.appID, function () {
+      console.log('init success')
+    }, (err) => {
+      console.error(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +51,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="video"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
